e2f3eb5a715c4f0944b54f5629d8e7926c9180bd
Josh Gao <josh@insolit.us>
2016-01-16 02:57:36 -0800

    Update submodules when cloning/pulling.

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,17 +3,20 @@
 # jmgao/dotfiles ellipsis package
 
 # The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+pkg.install() {
+    git submodule init
+    git submodule update
+    fs.link_files $PKG_PATH
+}
 
 # pkg.push() {
 #     git.push
 # }
 
-# pkg.pull() {
-#     git.pull
-# }
+pkg.pull() {
+    git.pull
+    git submodule update --recursive
+}
 
 # pkg.installed() {
 #     git.status
